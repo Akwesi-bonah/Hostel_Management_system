a5d8b8d3e1a50990acebd26459646665a48c4eff
Akwesi-bonah <arhinbonnah@gmail.com>
2023-11-19 14:41:48 +0000

    student view added

--- web_flask/student_model/__init__.py
+++ web_flask/student_model/__init__.py
@@ -1,4 +1,4 @@
 from flask import Blueprint
 
-student_views = Blueprint('student_views', __name__, template_folder='student_templates', static_folder='static')
+student_views = Blueprint('student_views', __name__, template_folder='student_templates')
 from web_flask.student_model.defualt import *
--- web_flask/student_model/defualt.py
+++ web_flask/student_model/defualt.py
@@ -1,10 +1,43 @@
 from web_flask.student_model import student_views
 from flask import render_template
 
+
 @student_views.route('/default', methods=['GET'])
 def default():
     """
      Display default site student
     """
-
     return render_template('default.html')
+
+
+@student_views.route('/default/student', methods=['GET'])
+def dashboard():
+    return render_template('Sbase.html')
+
+
+@student_views.route('/default/student/profile', methods=['GET'])
+def student_profile():
+    """Student profile"""
+
+    return render_template('Sprofile.html')
+
+
+@student_views.route('/default/student/booking', methods=['GET'])
+def booking():
+    """ booking for rooms"""
+
+    return render_template('booking.html')
+
+
+@student_views.route('/default/student/mybooking', methods=['GET'])
+def my_bookings():
+    """ display student booking infor"""
+
+    return render_template('mybooking.html')
+
+
+@student_views.route('/default/student/mybooking/details', methods=['GET'])
+def booking_details():
+    """ display student booking infor"""
+
+    return render_template('details.html')
