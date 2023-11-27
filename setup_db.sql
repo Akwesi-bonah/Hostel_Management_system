00b42a1b9064d98436e6dd5230bcf6680a094dc7
Akwesi-bonah <arhinbonnah@gmail.com>
2023-11-27 16:05:51 +0000

    restored

--- setup_db.sql
+++ setup_db.sql
@@ -1,7 +1,7 @@
 -- prepares a MySQL server for the project
 
-CREATE DATABASE IF NOT EXISTS hms_db;
-CREATE USER IF NOT EXISTS 'akwesi'@'localhost' IDENTIFIED BY 'akwesi_pwd';
-GRANT ALL PRIVILEGES ON `hms_db`.* TO 'akwesi'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'akwesi'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
