2707add5c5163381548836f0660f68b6ba4d3b6f
Akwesi-bonah <arhinbonnah@gmail.com>
2023-12-06 00:55:39 +0000

    mail server setup 2

--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -32,7 +32,7 @@ def create_app():
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USERNAME'] = 'ranisminth@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'testingFlask@mail'
+    app.config['MAIL_PASSWORD'] = ''
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
     mail.init_app(app)
