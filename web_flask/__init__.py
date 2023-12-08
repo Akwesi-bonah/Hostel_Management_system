9798cbae646633c5e291be00b677ce95fcf48899
Akwesi-bonah <arhinbonnah@gmail.com>
2023-12-08 20:37:28 +0000

    error code handled

--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -4,7 +4,7 @@ flask setup
 """
 import os
 
-from flask import Flask, session, g, redirect, url_for
+from flask import Flask, session, g, redirect, url_for, render_template
 from web_flask.componet.views import staff_view
 from web_flask.student_model import student_views
 from flask_session import Session
@@ -19,11 +19,14 @@ def create_app():
     app.config['SECRET_KEY'] = os.urandom(24)
     app.config['SESSION_COOKIE_NAME'] = 'staff_session'
     app.config['STUDENT_SESSION_COOKIE_NAME'] = 'student_session'
-    app.config["SESSION_PERMANENT"] = False
-    app.config["SESSION_TYPE"] = "filesystem"
+    # Session configurations
+    app.config['SESSION_COOKIE_NAME'] = 'staff_session'
+    app.config['SESSION_COOKIE_SECURE'] = True
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.config['SESSION_FILE_DIR'] = './.flask_session/'
     app.config['SESSION_KEY_PREFIX'] = 'session:'
-    app.config['SESSION_COOKIE_SECURE'] = False
+    app.config['PERMANENT_SESSION_LIFETIME'] = 3600
+    app.config['SESSION_REFRESH_EACH_REQUEST'] = True
 
     app.config['PAYSTACK_SECRET_'] = 'sk_test_7530309aeb43b700e14cf312de735ad407747903'
     app.config['PAYSTACK_PUBLIC_KEY'] = 'pk_test_4ccdf50310beaaefdde4febbcef5fee8fbbd7011'
@@ -37,6 +40,22 @@ def create_app():
     app.config['MAIL_USE_SSL'] = False
     mail.init_app(app)
     Session(app)
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('404.html'), 404
+
+    @app.errorhandler(500)
+    def internal_error(error):
+        return render_template('500.html'), 500
+
+    @app.errorhandler(Exception)
+    def unhandled_exception(e):
+        return render_template('error.html',
+                               error=str(e)), 500
+
+    if __name__ == '__main__':
+        app.run(debug=True)
 
     app.register_blueprint(staff_view, url_prefix='/staff/')
     app.register_blueprint(student_views, url_prefix='/')
